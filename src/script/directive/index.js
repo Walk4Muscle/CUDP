4c877571b4bbde385168089e83ca3d8b337f03c5
Gary Liu (Wicresoft) <v-guiliu@microsoft.com>
2017-03-21 18:28:47 +0800

    update total count in pagination

--- src/script/directive/index.js
+++ src/script/directive/index.js
@@ -1,9 +1,33 @@
-
 let app = angular.module('app.Directive', []);
 
-app.directive('menuLink',require('./MenuLink.directive.js'))
-app.directive('menuToggle',require('./MenuToggle.directive.js'))
-app.directive('statusDropdown',require('./StatusDropdown.directive.js'))
+app.directive('menuLink', require('./MenuLink.directive.js'))
+app.directive('menuToggle', require('./MenuToggle.directive.js'))
+app.directive('statusDropdown', require('./StatusDropdown.directive.js'))
 app.directive('selectPage', require('./selectPage.directive.js'));
 
+app.directive('stSummary', function () {
+    'use strict';
+    return {
+        require: '^stTable',
+        template: '<span layout-padding>Showing {{ stRange.from }}-{{ stRange.to }} of {{ totalItemCount }} records</span>',
+        link: function (scope, element, attrs, stTable) {
+            scope.stRange = {
+                from: null,
+                to: null
+            };
+            scope.$watch('totalItemCount', () => {
+                count()
+            })
+            scope.$watch('currentPage', () => {
+                count()
+            });
+            let count = ()=>{
+                var pagination = stTable.tableState().pagination;
+                scope.stRange.from = pagination.start + 1;
+                scope.stRange.to = scope.currentPage === pagination.numberOfPages ?
+                    pagination.totalItemCount : (scope.stRange.from + scope.stItemsByPage - 1);
+            }
+        }
+    };
+})
 module.exports = 'app.Directive';
